9f8253488a5873410c6c94d117d399807cd2c46d
801cucumber <psyche.delicha@gmail.com>
2016-04-20 22:02:33 +1200

    Add auto-open functionality

--- js/site.js
+++ js/site.js
@@ -1,3 +1,12 @@
+var url = window.location.href;
+try {
+  var hash = url.substring(url.indexOf('#') + 1);
+  if (hash.length) {
+    console.log(hash);
+    var content = $('#' + hash + '-content').slideDown(200);
+  }
+} catch (err) {}
+
 $('.level-toggle').click(function() {
   $('.level-content').slideUp(200);
   $(this).closest('.level').find('.level-content').slideToggle(200);
