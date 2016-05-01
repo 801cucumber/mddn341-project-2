8b434269a7d36f8e14155275f01c62bb399ec13b
Jean <psyche.delicha@gmail.com>
2016-05-01 16:16:37 +1200

    Added files via upload

--- js/site.js
+++ js/site.js
@@ -10,4 +10,23 @@ try {
 $('.level-toggle').click(function() {
   $('.level-content').slideUp(200);
   $(this).closest('.level').find('.level-content').slideToggle(200);
+  $('map').imageMapResize();
+});
+
+
+$('area').on('click', function() {
+  var name = $(this).attr('name');
+
+  $('#overlay').fadeIn(300);
+  if (name === "level1-store") {
+    $('#level1-popup-store').fadeIn(300);
+  } else if (name === "level1-cafe") {
+    $('#level1-popup-cafe').fadeIn(300);
+    $(window).trigger('resize');
+  }
+});
+
+$('#overlay').click(function() {
+  $('.popup').fadeOut(300);
+  $('#overlay').fadeOut(300);
 });
